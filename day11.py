d9d54bdfacf721ed382749068645b7755e9c30b1
Mailboy4 <asana3@gmail.com>
2025-10-10 14:35:12 +0300

    github

---
+++ day11.py
@@ -0,0 +1,18 @@
+    #important
+  #1
+#print ( 6 + 4 * 2) 
+#print ((6 + 4) * 2) 
+#print ( 20 - (3 + 2) * 2)
+    #2
+
+     # importnt
+
+#first_name= "km"
+#last_name= "4"
+#print (first_name, last_name,) 
+     #1
+#num1= 20 
+#num2 =10 
+#difference = num1-num2 
+#print("difference is", differencce )
+     #2
