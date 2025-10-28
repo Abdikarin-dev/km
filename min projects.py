12f836efcc8495c9cd9bd2e58eff7f3812f20eb7
Maliboy4 <4sane19@gmail.com>
2025-10-28 15:41:12 +0300

    Add files via upload

---
+++ min projects.py
@@ -0,0 +1,12 @@
+#My beginner coding projects and exercise .
+
+#Grade Checker
+grade = 75
+if grade >= 90:
+    print("Excellent")
+elif grade >= 70:
+    print("Good")
+elif 50 <= grade <= 69:
+    print("Pass")
+else:
+    print("Fail")
