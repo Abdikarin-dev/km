1442b143739bf617a825f178173ae9b4bc920353
Maliboy4 <4sane19@gmail.com>
2025-11-18 19:29:57 +0300

    Update min projects.py

--- min projects.py
+++ min projects.py
@@ -1,13 +1,3 @@
 #My beginner coding projects and exercise .
 
-#Grade Checker
-grade = int(input("Enter your grade (0–100): "))
-if grade >= 90:
-    print("Excellent")
-elif grade >= 70:
-    print("Good")
-elif 50 <= grade <= 69:
-    print("Pass")
-else:
-    print("Fail")
 
